f04663b6bcd681eeee5b620627588a863a2e9419
Beniadem <aladagadem@gmail.com>
2022-10-09 15:18:10 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { useState } from "react";
 
 function App() {
   const [todoText, setTodoText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const changeIsDone = (id) => {
+    const searchedTodo = todos.find((item) => item.id === id);
+    const updatedTodo = {
+      ...searchedTodo,
+      isDone: !searchedTodo.isDone,
+    };
+    const filteredTodos = todos.filter((item) => item.id !== id);
+    setTodos([updatedTodo, ...filteredTodos]);
+    console.log(filteredTodos);
+  };
+  const doneText = (text) => {};
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -9,7 +21,25 @@ function App() {
       alert("text cannot be empty");
       return;
     }
-    console.log(todoText);
+    const hasTodos = todos.find((item) => item.text === todoText);
+    if (hasTodos !== undefined) {
+      alert("You already have this todo!");
+      return;
+    }
+    const newTodo = {
+      id: new Date().getTime(),
+      isDone: false,
+      text: todoText,
+      date: new Date(),
+    };
+    setTodos([newTodo, ...todos]);
+    setTodoText("");
+
+    /*const newTodoList=todos
+    newTodoList.push(newTodo)
+    setTodos(newTodoList)*/
+
+    console.log(newTodo);
   };
 
   return (
@@ -29,6 +59,23 @@ function App() {
           </button>
         </div>
       </form>
+      {todos.length <= 0 ? (
+        <p className="text-center my-5">You don't have todos yet.</p>
+      ) : (
+        <>
+          {todos.map((item) => (
+            <div className="alert alert-secondary d-flex justify-content-between align-items-center">
+              <p>{item.text}</p>
+              <button
+                onClick={() => changeIsDone(item.id)}
+                className="btn btn-sm btn-success "
+              >
+                {item.isDone === false ? "Done" : "Undone"}
+              </button>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
